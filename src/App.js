7d6a551b2301eb1912c9ca4a0c43af9dfd19fbbe
Sumant9761 <sumantkumaryadav7037@gmail.com>
2026-01-12 17:53:09 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
-  return (
-    <div className="text-3xl` font-bold underline">
-      Namaste World!, Welcome to React JS.
-    </div>
-  );
+  return <Body />;
 }
 
 export default App;
